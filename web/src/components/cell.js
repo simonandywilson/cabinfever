50eae6305906ecab5629e6a700bec0c8b5923443
Simon Wilson <contact@simonwilson.design>
2021-06-03 17:03:25 +0100

    Corrected text

--- web/src/components/cell.js
+++ web/src/components/cell.js
@@ -1,12 +1,5 @@
 import React, {useState} from 'react'
 import * as style from "../styles/cell.module.css";
-import Cab from "../images/cab.svg"
-import Bin from "../images/bin.svg";
-import Fev from "../images/fev.svg";
-import Ver from "../images/ver.svg";
-
-import Cabin from "../images/cabin.svg"
-
 
 const Cell = (props) => {
 const [visible, setVisible] = useState(false)
@@ -20,7 +13,7 @@ const [visible, setVisible] = useState(false)
             {visible ? <div className={style.colour}></div> : null}
             {!visible ? (
                 <div className={style.text}>
-                    <img className={style.image} src={Cab} alt="Cabin Fever" draggable="false" />
+                    <img className={style.image} src={`${props.order}.svg`} alt="Cabin Fever" draggable="false" />
                 </div>
             ) : null}
         </div>
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,24 +1,38 @@
-import * as React from "react"
-import * as style from "../styles/home.module.css"
+import * as React from "react";
+import * as style from "../styles/home.module.css";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Menu from "../components/menu";
-import Cell from "../components/cell"
+import Cell from "../components/cell";
 import Banner from "../components/banner";
 
-const Home = () => {
-  return (
-      <main className={style.container}>
-        <Menu />
-          <div className={style.wrapper}>
-              <div className={style.grid}>
-                  {[...Array(16)].map((e, i) =>
-                  <Cell key={i}/>
-      
-                  )}
-              </div>
-          </div>
-          <Banner />
-      </main>
-  );
-}
+const Home = ({ data }) => {
+    const content = data.sanityContent;
+    return (
+        <main className={style.container}>
+            <Menu />
+            <div className={style.wrapper}>
+                <div className={style.grid}>
+                    {content.order.map((order, i) => {
+                        return <Cell key={i} order={order}/>;
+                    })}
 
-export default Home
+                    {/* {[...Array(16)].map((e, i) => (
+                        <Cell key={i} />
+                    ))} */}
+                </div>
+            </div>
+            <Banner />
+        </main>
+    );
+};
+
+export default Home;
+
+export const query = graphql`
+    query {
+        sanityContent {
+            order
+        }
+    }
+`;
