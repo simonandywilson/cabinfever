601821a5597327d170779199272782c34fd6ef84
Simon Wilson <contact@simonwilson.design>
2021-06-03 14:11:55 +0100

    Added netlify.toml

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,33 +1,34 @@
 module.exports = {
-  siteMetadata: {
-    title: "Cabin Fever",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-sanity",
-      options: {
-        projectId: "m8oqv0ie",
-        dataset: "production",
-      },
+    siteMetadata: {
+        title: "Cabin Fever",
+        siteUrl: "https://www.cabinfever24hours.com",
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-source-sanity",
+            options: {
+                projectId: "m8oqv0ie",
+                dataset: "production",
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
